7963aa632b2860a2fd082c862856c0109536af9a
Your Name <you@example.com>
2026-03-22 16:07:51 +0300

    22.03.26d

--- Player/stats.gd
+++ Player/stats.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 func _on_health_regen_timeout() -> void:
 	if Global.player_health < 100:
 		Global.player_health += 1
-		self.value = Global.player_heaalth
+		self.value = Global.player_health
